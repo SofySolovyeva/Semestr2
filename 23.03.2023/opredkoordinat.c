1347abc57d28e2cfca0281b2ca133a363d6f439f
sofya <sofysolovyeva@gmail.com>
2023-03-23 13:12:52 +0300

    23/03/2023

---
+++ 23.03.2023/opredkoordinat.c
@@ -0,0 +1,38 @@
+#include <stdio.h> 
+// #include <stdlib.h> 
+#include <locale.h>
+
+int Bissectrice(double x, double y);
+int Gorisontal(double x, double y);
+int Wertical(double x, double y);
+
+int main(){
+    double x = 5;
+    double y = 5;
+    printf("%lf\n", x);
+    printf("%lf\n", y);
+    printf("%d", Wertical && Gorisontal || Bissectrice );
+    return 0;
+}
+
+int Bissectrice(double x, double y){
+    int res = -1;
+    res = (y <= -x);
+    return res;
+}
+
+int Gorisontal(double x, double y){
+    int res = -1;
+    res = (y > 3);
+    return res;
+}
+
+int Wertical(double x, double y){
+    int res = -1;
+    res = (x > 4);
+    return res;
+}
+/*
+На вход получаем x, y
+На выход получаем 1, 0
+*/
